d9e2cf5cffcf092cf86c07cdf584f8b7c3e07d88
danielbenitobravo <danielbenitobravo@gmail.com>
2024-02-22 08:35:31 -0800

    get and show data

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { TestService } from '../../services/test.service';
-import { Observable, debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs';
+import { Observable, debounceTime, distinctUntilChanged, map, of, switchMap } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
@@ -16,6 +16,8 @@ export class ListComponent {
   form: FormGroup = new FormGroup({
     searchBar: new FormControl()
   });
+  results$: Observable<any> | undefined;
+  data: any[] = [];
 
   constructor(private _testService: TestService) {
     this.form.get('searchBar')?.valueChanges.
@@ -23,8 +25,15 @@ export class ListComponent {
       debounceTime(400),
       distinctUntilChanged(),
       switchMap((search) => this._testService.getData(search))
-    )
-    .subscribe(res => console.log('response: ', res))
+    ).subscribe((res: any) => {
+      this.data = res.data.map((anime: any) => {
+        return {
+          title: anime.title,
+          img: anime.images.jpg.image_url
+        }
+      });
+    });
+    console.log(this.data)
   }
 
 }
